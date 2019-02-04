d889e939e529f16ebfafda21c96784837ea0999f
MeganaChinala <m.chinalachaiagari@gmail.com>
2019-02-04 15:57:47 -0600

    made changes to errors in searchmethod - due to segmentation fault

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -37,14 +37,15 @@ bool LinkedList<T>::search(T value) const
 {
 	Node<T>* temp = m_front;
 	bool isFound = false;
-	do
+
+	while (temp != nullptr)
 	{
 		if (temp->getValue() == value)
 		{
 			return (true);
 		}
 		temp = temp->getNext();
-	} while (temp->getNext() != nullptr);
+	}
 
 	return(isFound);
 }
